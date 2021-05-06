819cee2efb741d767a657faddb215789f2751eaa
Kira <Kira@fanruan.com>
2021-05-06 15:01:01 +0800

    KERNEL-7297 chore: 适配mongourl

--- app/server/src/config/typeorm/typeorm.ts
+++ app/server/src/config/typeorm/typeorm.ts
@@ -1,16 +1,13 @@
 import { TYPEORM } from '@environment';
 import { Injectable, Logger } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import {
-    ConnectionOptions,
-    createConnection,
-    getMetadataArgsStorage,
-} from 'typeorm';
+import { createConnection, getMetadataArgsStorage } from 'typeorm';
+import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
 
 @Injectable()
 export class TypeormService implements TypeOrmOptionsFactory {
     async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
-        const options: ConnectionOptions = {
+        const options: MongoConnectionOptions = {
             ...TYPEORM,
             type: 'mongodb',
             entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
@@ -20,6 +17,8 @@ export class TypeormService implements TypeOrmOptionsFactory {
             useUnifiedTopology: true,
             logging: true,
         };
+
+        Logger.log(options, 'TYPEORM', false);
 
         createConnection(options)
             .then(() => {
